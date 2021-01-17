e021da4d9cef64e31f740447f4253c1957d55ee6
Robin Genz <mail@robingenz.dev>
2021-01-17 22:26:58 +0100

    wip

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,5 +5,47 @@ declare module '@capacitor/core' {
 }
 
 export interface AppUpdatePlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  /**
+   * Supported platform(s): Android, iOS  
+   * Returns app update informations.  
+   */
+  getAppUpdateInfo(): Promise<AppUpdateInfo>;
+  /**
+   * Supported platform(s): Android, iOS  
+   * Opens the app store entry of the app in the Play Store (Android) or App Store (iOS).  
+   */
+  openAppStore(): Promise<void>;
+  /**
+   * Supported platform(s): Android  
+   * Request an in-app update.  
+   */
+  startInAppUpdate(): Promise<void>;
+}
+
+export interface AppUpdateInfo {
+  /**
+   * Supported platform(s): Android, iOS  
+   * Version code (Android) or CFBundleShortVersionString (iOS) of the currently installed app version.  
+   */
+  currentVersion: string;
+  /**
+   * Supported platform(s): Android, iOS  
+   * Version code (Android) or CFBundleShortVersionString (iOS) of the update.  
+   */
+  availableVersion?: string;
+  /**
+   * Supported platform(s): iOS  
+   * Release date of the update in ISO 8601 (UTC) format.  
+   */
+  availableVersionReleaseDate?: string;
+  /**
+   * Supported platform(s): Android, iOS  
+   * `true` if the available version is higher than the current one, otherwise `false`.  
+   */
+  updateAvailable: boolean;
+  /**
+   * Supported platform(s): Android, iOS  
+   * Package name (Android) or Bundle identifier (iOS).  
+   */
+  packageName: string;
 }
