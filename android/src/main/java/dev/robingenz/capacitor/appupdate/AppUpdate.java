14165fbb764b3c1571296aed105bb0d8a0489ee7
Robin Genz <mail@robingenz.dev>
2021-02-19 21:18:49 +0100

    wip

--- android/src/main/java/dev/robingenz/capacitor/appupdate/AppUpdate.java
+++ android/src/main/java/dev/robingenz/capacitor/appupdate/AppUpdate.java
@@ -2,6 +2,8 @@ package dev.robingenz.capacitor.appupdate;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 
 import com.getcapacitor.JSObject;
@@ -27,17 +29,30 @@ public class AppUpdate extends Plugin {
     public void getAppUpdateInfo(PluginCall call) {
         Task<AppUpdateInfo> appUpdateInfoTask = this.appUpdateManager.getAppUpdateInfo();
         appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
-
+            PackageInfo pInfo = null;
+            try {
+                pInfo = this.getPackageInfo();
+            } catch (PackageManager.NameNotFoundException e) {
+                call.reject("Unable to get App Info");
+                return;
+            }
+            JSObject ret = new JSObject();
+            ret.put("currentVersion", pInfo.versionCode);
+            ret.put("availableVersion", appUpdateInfo.availableVersionCode());
+            ret.put("updateAvailable", appUpdateInfo.updateAvailability());
+            ret.put("updatePriority", appUpdateInfo.updatePriority());
+            ret.put("appId", pInfo.packageName);
+            call.resolve(ret);
         });
-
-        JSObject ret = new JSObject();
-        ret.put("test", true);
-        call.resolve(ret);
+        appUpdateInfoTask.addOnFailureListener(failure -> {
+            String message = failure.getMessage();
+            call.reject(message);
+        });
     }
 
     @PluginMethod
     public void openAppStore(PluginCall call) {
-        final String packageName = this.getContext().getPackageName();
+        String packageName = this.getContext().getPackageName();
         Intent launchIntent  = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName));
         try {
             this.getBridge().getActivity().startActivity(launchIntent);
@@ -46,5 +61,10 @@ public class AppUpdate extends Plugin {
             this.getBridge().getActivity().startActivity(launchIntent);
         }
         call.resolve();
+    }
+
+    private PackageInfo getPackageInfo() throws PackageManager.NameNotFoundException {
+        String packageName = this.getContext().getPackageName();
+        return this.getContext().getPackageManager().getPackageInfo(packageName, 0);
     }
 }
